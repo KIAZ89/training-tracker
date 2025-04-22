3500e4aa9a6c5edb1a5d73dc2d721a2744da333f
KIAZ89 <nico.perlbach@gmx.de>
2025-04-22 07:07:18 +0200

    Add basic TrainingTracker component

--- src/TrainingTracker.jsx
+++ src/TrainingTracker.jsx
@@ -1,1 +1,11 @@
-<PLACEHOLDER_FOR_TRAINING_TRACKER_COMPONENT>
+import React from "react";
+
+export default function TrainingTracker() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Training Tracker</h1>
+      <p>Willkommen zu deinem persönlichen Trainings-Tracker!</p>
+      {/* Hier kannst du später deine Trainingsdaten und Logik einbauen */}
+    </div>
+  );
+}
