c247eeddf348e5384640ecba6cffec8ff549834f
KIAZ89 <nico.perlbach@gmx.de>
2025-04-22 06:42:32 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TrainingTracker from './TrainingTracker.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TrainingTracker />
+  </React.StrictMode>
+)
